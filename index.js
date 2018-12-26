4ac339a87a99e92047314f6f0fadddf9fc0067be
sandyM24 <iambansalsandeep@outlook.com>
2018-12-26 12:33:23 +0530

    Env, Configuration, Debugging, Template Engine, Middleware

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const debug = require('debug')('app:startup');
 const Joi = require('joi');
+const config = require('config');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 
+const logger = require('./custom_middleware/logger');
+
+// Template Engine ( we don't need to require )
+app.set('view engine', 'pug');
+app.set('views', './views'); // by default optional
+
+// Configuration
+console.log(`Application name: ${config.get('name')}`);
+console.log(`Mail host: ${config.get('mail.host')}`);
+console.log(`Mail password: ${config.get('mail.password')}`); // For this add "export app_password=1234"
+
+// Environment
+// console.log(process.env.NODE_ENV); // By default it return undefined
+console.log(app.get('env')); // By default it return 'development'
+
+// Built in Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); // We can access 'localhost:3000/readme.txt'
+
+// Custom Middleware
+app.use(logger);
+
+// Third party middleware
+app.use(helmet());
+if (app.get('env') === 'development') {
+  // Only enanled in development env by using enviornment.
+  app.use(morgan('tiny'));
+  debug('Morgan Enabled');
+}
 
 const courses = [
   { id: 1, name: 'NodeJS' },
@@ -12,7 +44,8 @@ const courses = [
 ];
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // res.send('Hello World!');
+  res.render('index', { title: 'My Express APP', h1: 'Hello World! How r u?' });
 });
 
 app.get('/api/courses', (req, res) => {
